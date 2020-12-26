e32a03b08da6864f40ac1ddedef01f79f08e6225
muskaan.agrawal <muskaan.agrawal@pearson.com>
2020-12-26 21:13:28 +0530

    second commit

--- src/test/java/com/cucumber/apitesting/ApiTestingApplicationTests.java
+++ src/test/java/com/cucumber/apitesting/ApiTestingApplicationTests.java
@@ -1,19 +1,8 @@
 package com.cucumber.apitesting;
 
-import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringRunner;
-
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-import io.cucumber.spring.CucumberContextConfiguration;
 
 @SpringBootTest
-@RunWith(Cucumber.class)
-@CucumberOptions(features = "classpath:Feature",
-glue = {"com.cucumber.apitesting"})
 class ApiTestingApplicationTests {
 
 }
